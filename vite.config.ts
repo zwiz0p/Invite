f1aa129187c5c26ea971f9d399967cd556eeef09
zwiz0p <zwiz0p@gmail.com>
2026-02-03 14:08:09 +0530

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Invite/" : "/",
+ 
 
   plugins: [react()],
 
@@ -13,8 +13,5 @@ export default defineConfig(({ mode }) => ({
     },
   },
 
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  
 }));
