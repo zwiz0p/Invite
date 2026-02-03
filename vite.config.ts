0b12ce864fae6f841b2fd08b5056131b662e52d6
zwiz0p <zwiz0p@gmail.com>
2026-02-03 12:33:48 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/Invite/",   // 👈 GitHub repo name
+  base: "/",   // 👈 GitHub repo name
 
   server: {
     host: "::",
